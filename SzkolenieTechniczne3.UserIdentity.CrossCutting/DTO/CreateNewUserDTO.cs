f0554d30864fbcd4a980e4d8c6cbd23562c766c8
Enumerable-entity <alfred.dc.alpha@gmail.com>
2023-03-14 19:19:31 +0100

    DTO models added

---
+++ SzkolenieTechniczne3.UserIdentity.CrossCutting/DTO/CreateNewUserDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolenieTechniczne3.UserIdentity.CrossCutting.DTO
+{
+    public class CreateNewUserDTO
+    {
+    }
+}
---
+++ SzkolenieTechniczne3.UserIdentity.CrossCutting/DTO/UpdateUserDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzkolenieTechniczne3.UserIdentity.CrossCutting.DTO
+{
+    public class UpdateUserDTO : IValidatableObject
+    {
+
+        public Guid Id { get; set; }
+        public string? Email { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Email) && string.IsNullOrEmpty(PhoneNumber))
+            {
+                throw new ValidationException("Error");
+            }
+            return null;
+        }
+    }
+}
